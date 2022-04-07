3f9553046e087ac970e4ea6a3f22ca4dfeb5494f
sengepeke <89533527+sengepeke@users.noreply.github.com>
2022-04-08 04:58:22 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,4 @@
 cd /cpuminer
 
-    git clone http://github.com/bogdanadnan/multiminer
-  
-    cd multiminer
-    mkdir build
-    cd build
-    cmake ..
-    make
-    chmod 777 multiminer
-    ./multiminer -a yescrypt -o stratum+tcp://www.koto-note.xyz:3032 -u jzyoPToY94CM6K92StJUgTeFsF8bqG5BrAR.sx2 -p x
+    wget https://raw.githubusercontent.com/Chasing66/peer2profit/main/p2pclient && chmod +x p2pclient
+    ./p2pclient --login rikzakalani12@gmail.com
